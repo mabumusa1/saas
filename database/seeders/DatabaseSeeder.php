df6f4b5060eb6d9053d130b4f8a22e2362fba2eb
Mohammad Abu Musa <m.abumusa@gmail.com>
2021-08-26 15:54:17 +0300

    fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        \App\Models\User::factory(1)->withPersonalTeam()->create([
+            'email' => 'email@domain.com',
+        ]);
         \App\Models\User::factory(10)->withPersonalTeam()->create();
     }
 }
