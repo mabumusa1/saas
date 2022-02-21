28e18a145162dff2764e6dc7d541b8e15a91799c
Salem Code <salemcode8@gmail.com>
2022-02-21 09:01:10 +0200

    Sites Search

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\StoreSiteRequest;
 use App\Http\Requests\UpdateSiteRequest;
 use App\Models\Account;
 use App\Models\Site;
+use Illuminate\Http\Request;
 
 class SiteController extends Controller
 {
@@ -25,9 +26,16 @@ class SiteController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Account $account)
+    public function index(Account $account, Request $request)
     {
-        return view('sites.index', ['sites' => $account->sites]);
+        $sites = $account->sites();
+        if ($request->filled('q')) {
+            $sites->where('name', 'like', '%'.$request->q.'%')->orWhereHas('installs', function ($query) use ($request) {
+                $query->where('name', 'like', '%'.$request->q.'%');
+            });
+        }
+
+        return view('sites.index', ['sites' => $sites->get()]);
     }
 
     /**
