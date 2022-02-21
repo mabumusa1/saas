482af193c55eff227673ba91f194e50c7b64928c
Salem Code <salemcode8@gmail.com>
2022-02-21 09:01:10 +0200

    Sites Search

--- resources/views/sites/index.blade.php
+++ resources/views/sites/index.blade.php
@@ -7,44 +7,56 @@
                         <h1>Sites</h1>
                         <a href="{{ route('sites.create', $currentAccount->id) }}" class="btn btn-primary">Add Site</a>
                     </div>
+                    <div class="col-5 mb-4">
+                        <h5 class="text-muted">Search for site, domain, environment</h4>
+                            <form class="input-group mb-3">
+                                <input type="text" class="form-control border-right-0" name="q" value="{{ request()->get('q') }}"/>
+                                <span class="input-group-text bg-transparent " id="basic-addon2"><i class="bi bi-search"></i></span>
+                              </form>
+                    </div>
                     <div class="table-responsive">
+
                         <table class="table table-rounded table-row-bordered border gy-7 gs-7">
                             <thead>
-                            <tr class="fw-bold fs-6 text-gray-800 border-bottom-2 border-gray-200">
-                                <th>Site Name</th>
-                                <th>Actions</th>
-                            </tr>
+                                <tr class="fw-bold fs-6 text-gray-800 border-bottom-2 border-gray-200">
+                                    <th>Site Name</th>
+                                    <th>Actions</th>
+                                </tr>
                             </thead>
                             <tbody>
-                                @foreach ($sites as $site )
+                                @foreach ($sites as $site)
                                     <tr>
                                         <td>{{ $site->name }}</td>
-                                        <td><a href="{{ route('sites.edit',[$currentAccount->id, $site->id]) }}">Edit</a></td>
-                                    </tr>
-                                    @foreach ( $site->installs as $install )
-                                    <tr>
-                                        <td class="table-light">
-                                            @switch($install->type)
-                                                @case('prd')
-                                                    <span class="badge badge-success">PRD</span>
-                                                @break
-                                                @case('stg')
-                                                    <span class="badge badge-light-success">STG</span>
-                                                @break
-                                                @case('dev')
-                                                    <span class="badge badge-light-dark">DEV</span>
-                                                @break
-                                            @endswitch
-                                            <p>{{ $install->name }}</p>
-                                        </td>
-                                        <td class="table-light">
-                                            <div class="btn-group" role="group" aria-label="Basic example">
-                                                <a class="btn btn-sm btn-primary" href="#">Backup Install</a>
-                                                <a class="btn btn-sm btn-primary" href="#"> Clear Cache</a>
-                                                <a class="btn btn-sm btn-danger" href="#"> Delete Install</a>
-                                              </div>                                            
+                                        <td><a
+                                                href="{{ route('sites.edit', [$currentAccount->id, $site->id]) }}">Edit</a>
                                         </td>
                                     </tr>
+                                    @foreach ($site->installs as $install)
+                                        <tr>
+                                            <td class="table-light">
+                                                @switch($install->type)
+                                                    @case('prd')
+                                                        <span class="badge badge-success">PRD</span>
+                                                    @break
+
+                                                    @case('stg')
+                                                        <span class="badge badge-light-success">STG</span>
+                                                    @break
+
+                                                    @case('dev')
+                                                        <span class="badge badge-light-dark">DEV</span>
+                                                    @break
+                                                @endswitch
+                                                <p>{{ $install->name }}</p>
+                                            </td>
+                                            <td class="table-light">
+                                                <div class="btn-group" role="group" aria-label="Basic example">
+                                                    <a class="btn btn-sm btn-primary" href="#">Backup Install</a>
+                                                    <a class="btn btn-sm btn-primary" href="#"> Clear Cache</a>
+                                                    <a class="btn btn-sm btn-danger" href="#"> Delete Install</a>
+                                                </div>
+                                            </td>
+                                        </tr>
                                     @endforeach
                                 @endforeach
                             </tbody>
