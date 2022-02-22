826fdba56a5126d571620aad8778e427285579a2
Vigen Harutyunyan <harvigen10@gmail.com>
2022-02-22 11:55:56 +0400

    user controller feature test

---
+++ database/factories/AccountUserFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class AccountUserFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' =>  $this->faker->uuid(),
+            'account_id' =>  $this->faker->uuid(),
+            'role' =>  'owner',
+        ];
+    }
+}
---
+++ tests/Feature/Controllers/UserControllerTest.php
@@ -0,0 +1,217 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use App\Models\Account;
+use App\Models\AccountUser;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Jetstream\Jetstream;
+use Tests\TestCase;
+
+/**
+ * @see \App\Http\Controllers\AccountController
+ */
+class UserControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function index_displays_view()
+    {
+        $this->actingAs($user = User::factory()->create());
+
+        $account = Account::factory()->create();
+
+        $response = $this->get(route('users.index', $account));
+
+        $response->assertOk();
+        $response->assertViewIs('user.index');
+        $response->assertViewHas('users');
+    }
+
+    /**
+     * @test
+     */
+    public function create_displays_view()
+    {
+        $this->actingAs($user = User::factory()->create());
+
+        $account = Account::factory()->create();
+
+        $response = $this->get(route('users.create', $account));
+
+        $response->assertOk();
+        $response->assertViewIs('user.create');
+        $response->assertViewHas('account');
+    }
+
+    /**
+     * @test
+     */
+    public function user_store_fail_without_last_name()
+    {
+        $this->actingAs($user = User::factory()->create());
+
+        $account = Account::factory()->create();
+
+        $response = $this->post(route('users.store', $account), [
+            'first_name' => 'First Name',
+            'last_name' => '',
+            'email' => 'test@example.com',
+            'role' => 'test@example.com',
+        ]);
+
+        $this->assertEquals($response->getStatusCode(), 302);
+        $this->assertEquals(session('errors')->get('last_name')[0], 'The last name field is required.');
+    }
+
+    /**
+     * @test
+     */
+    public function user_store()
+    {
+        $this->actingAs($user = User::factory()->create());
+
+        $account = Account::factory()->create();
+
+        $response = $this->post(route('users.store', $account), [
+            'first_name' => 'First Name',
+            'last_name' => 'Last Name',
+            'email' => 'test@example.com',
+            'role' => 'owner',
+            'password' => 'password',
+        ]);
+
+        $this->assertEquals($response->getStatusCode(), 302);
+        $this->assertEquals(session('message'), 'User successfully created!');
+    }
+
+    /**
+     * @test
+     */
+    public function edit_displays_view()
+    {
+        $this->actingAs($user = User::factory()->create());
+
+        $account = Account::factory()->create();
+        $user = User::factory()->create();
+
+        AccountUser::factory()->create([
+            'account_id' => $account->id,
+            'user_id' => $user->id,
+            'role' => 'owner',
+        ]);
+
+        $response = $this->get(route('users.edit', ['account' => $account, 'user' => $user]));
+
+        $response->assertOk();
+        $response->assertViewIs('user.edit');
+        $response->assertViewHas('account');
+        $response->assertViewHas('user');
+    }
+
+    /**
+     * @test
+     */
+    public function user_update_fail_without_last_name()
+    {
+        $this->actingAs($user = User::factory()->create());
+
+        $account = Account::factory()->create();
+
+        $response = $this->put(route('users.update', ['account' => $account, 'user' => $user]), [
+            'first_name' => 'First Name',
+            'last_name' => '',
+            'email' => 'test@example.com',
+            'role' => 'test@example.com',
+        ]);
+
+        $this->assertEquals($response->getStatusCode(), 302);
+        $this->assertEquals(session('errors')->get('last_name')[0], 'The last name field is required.');
+    }
+
+    /**
+     * @test
+     */
+    public function user_update()
+    {
+        $this->actingAs($user = User::factory()->create());
+
+        $account = Account::factory()->create();
+
+        $user = User::factory()->create();
+
+        AccountUser::factory()->create([
+            'account_id' => $account->id,
+            'user_id' => $user->id,
+            'role' => 'owner',
+        ]);
+
+        $response = $this->put(route('users.update', ['account' => $account, 'user' => $user]), [
+            'first_name' => 'First Name',
+            'last_name' => 'Last Name',
+            'email' => 'test@example.com',
+            'role' => 'owner',
+            'password' => 'password',
+        ]);
+
+        $this->assertEquals($response->getStatusCode(), 302);
+        $this->assertEquals(session('message'), 'User successfully updated!');
+    }
+
+    /**
+     * @test
+     */
+    public function user_destroy_only_if_account_has_one_owner()
+    {
+        $this->actingAs($user = User::factory()->create());
+
+        $account = Account::factory()->create();
+
+        $user = User::factory()->create();
+
+        AccountUser::factory()->create([
+            'account_id' => $account->id,
+            'user_id' => $user->id,
+            'role' => 'owner',
+        ]);
+
+        $response = $this->delete(route('users.destroy', ['account' => $account, 'user' => $user]));
+
+        $this->assertEquals($response->getStatusCode(), 302);
+        $this->assertEquals(session('message'), 'Sorry  you  can not delete this user!');
+    }
+
+    /**
+     * @test
+     */
+    public function user_destroy()
+    {
+        $this->actingAs($user = User::factory()->create());
+
+        $account = Account::factory()->create();
+
+        $user = User::factory()->create();
+
+        AccountUser::factory()->create([
+            'account_id' => $account->id,
+            'user_id' => $user->id,
+            'role' => 'owner',
+        ]);
+
+        $userSecond = User::factory()->create();
+
+        AccountUser::factory()->create([
+            'account_id' => $account->id,
+            'user_id' => $userSecond->id,
+            'role' => 'owner',
+        ]);
+
+        $response = $this->delete(route('users.destroy', ['account' => $account, 'user' => $user]));
+        $this->assertEquals($response->getStatusCode(), 302);
+        $this->assertEquals(session('message'), 'User successfully deleted!');
+    }
+}
