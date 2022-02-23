d0942fc1cb83ddf7354e5e6881b4fb16fe52d354
Mohammad Abu Musa <m.abumusa@gmail.com>
2022-02-23 04:55:16 +0000

    add groups test

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Group extends Model
@@ -32,5 +33,15 @@ class Group extends Model
     public function hasSite($site)
     {
         return $this->Sites->contains($site);
+    }
+
+    /**
+     * Get the Account that owns the Group.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function Account(): BelongsTo
+    {
+        return $this->belongsTo(Account::class);
     }
 }
