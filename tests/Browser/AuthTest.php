0feea2c255a25334afc7454cde2b549b1392ee2d
Mohammad Abu Musa <m.abumusa@gmail.com>
2022-02-25 01:16:25 +0300

    add dusk for testing ang GH

---
+++ tests/Browser/AuthTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use App\Models\Account;
+use App\Models\User;
+
+class AuthTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+ 
+    /**
+     * Can Login
+     *
+     * @return void
+     */
+    public function test_login()
+    {
+        Account::factory()->hasAttached(
+            User::factory()
+            ->sequence(
+                fn ($sequence) => ['email' => "email{$sequence->index}@domain.com"]
+            )
+        )->create();
+
+        $user = User::where('email','email0@domain.com')->first();
+
+        $this->browse(function ($browser) use ($user) {
+            $browser->screenshot('filename');
+            $browser->visit('/login')
+                    ->type('email', $user->email)
+                    ->type('password', 'password')
+                    ->press('Continue')
+                    ->assertPathIs('/1/dashboard');
+        });
+
+    }
+}
