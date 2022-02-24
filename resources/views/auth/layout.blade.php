3e56452dda1dd17a30773ba72bd69decd842beb6
Salem Code <salemcode8@gmail.com>
2022-02-24 17:34:43 +0200

    Remove Links from Auth Layout

--- resources/views/auth/layout.blade.php
+++ resources/views/auth/layout.blade.php
@@ -22,20 +22,6 @@
                 <!--end::Wrapper-->
             </div>
             <!--end::Content-->
-
-            <!--begin::Footer-->
-            <div class="d-flex flex-center flex-column-auto p-10">
-                <!--begin::Links-->
-                <div class="d-flex align-items-center fw-bold fs-6">
-                    <a href="{{ $theme->getOption("general", "about") }}" class="text-muted text-hover-primary px-2">{{ __('About') }}</a>
-
-                    <a href="{{ $theme->getOption('general', 'contact') }}" class="text-muted text-hover-primary px-2">{{ __('Contact Us') }}</a>
-
-                    <a href="{{ $theme->getOption('product', 'purchase') }}" class="text-muted text-hover-primary px-2">{{ __('Purchase') }}</a>
-                </div>
-                <!--end::Links-->
-            </div>
-            <!--end::Footer-->
         </div>
         <!--end::Authentication-->
     </div>
