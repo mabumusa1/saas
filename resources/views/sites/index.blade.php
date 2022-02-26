caf8eeb358a453a30776ad77aeaf17975013aae7
Salem Code <salemcode8@gmail.com>
2022-02-26 14:52:08 +0200

    Sites List icons

--- resources/views/sites/index.blade.php
+++ resources/views/sites/index.blade.php
@@ -54,10 +54,25 @@
                                             </td>
                                             <td class="text-center">
                                                 <div class="btn-group" role="group" aria-label="Basic example">
-                                                    <a class="btn btn-warning btn-sm"
-                                                        href="{{ route('sites.edit', [$currentAccount->id, $site->id]) }}">Edit</a>
+                                                    <a class="btn btn-warning btn-sm text-white"
+                                                        href="{{ route('sites.edit', [$currentAccount->id, $site->id]) }}">
+                                                        <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                                            <g id="Stockholm-icons-/-General-/-Edit" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                                            <rect id="bound" x="0" y="0" width="24" height="24"></rect>
+                                                            <path d="M7.10343995,21.9419885 L6.71653855,8.03551821 C6.70507204,7.62337518 6.86375628,7.22468355 7.15529818,6.93314165 L10.2341093,3.85433055 C10.8198957,3.26854411 11.7696432,3.26854411 12.3554296,3.85433055 L15.4614112,6.9603121 C15.7369117,7.23581259 15.8944065,7.6076995 15.9005637,7.99726737 L16.1199293,21.8765672 C16.1330212,22.7048909 15.4721452,23.3869929 14.6438216,23.4000848 C14.6359205,23.4002097 14.6280187,23.4002721 14.6201167,23.4002721 L8.60285976,23.4002721 C7.79067946,23.4002721 7.12602744,22.7538546 7.10343995,21.9419885 Z" id="Path-11" fill="#000000" fill-rule="nonzero" transform="translate(11.418039, 13.407631) rotate(-135.000000) translate(-11.418039, -13.407631) "></path>
+                                                        </g>
+                                                    </svg>
+                                                    </a>
                                                     <button class="btn btn-danger btn-sm btn-delete"
-                                                        data-target=".delete_form{{ $site->id }}">Delete</button>
+                                                        data-target=".delete_form{{ $site->id }}">
+                                                        <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                                            <g id="Stockholm-icons-/-General-/-Trash" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                                                <rect id="bound" x="0" y="0" width="24" height="24"></rect>
+                                                                <path d="M6,8 L6,20.5 C6,21.3284271 6.67157288,22 7.5,22 L16.5,22 C17.3284271,22 18,21.3284271 18,20.5 L18,8 L6,8 Z" id="round" fill="#fff" fill-rule="nonzero"></path>
+                                                                <path d="M14,4.5 L14,4 C14,3.44771525 13.5522847,3 13,3 L11,3 C10.4477153,3 10,3.44771525 10,4 L10,4.5 L5.5,4.5 C5.22385763,4.5 5,4.72385763 5,5 L5,5.5 C5,5.77614237 5.22385763,6 5.5,6 L18.5,6 C18.7761424,6 19,5.77614237 19,5.5 L19,5 C19,4.72385763 18.7761424,4.5 18.5,4.5 L14,4.5 Z" id="Shape" fill="#fff"></path>
+                                                            </g>
+                                                        </svg>
+                                                    </button>
 
                                                 </div>
                                                 <form
