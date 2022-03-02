141f58dff22f75840dad06fb0fa3e0cdbffb6498
Salem Code <salemcode8@gmail.com>
2022-03-02 13:47:24 +0200

    Checkout new UI

--- resources/views/payment/checkout.blade.php
+++ resources/views/payment/checkout.blade.php
@@ -19,14 +19,15 @@
             document.querySelectorAll('.purchase-button').forEach(function(btn) {
                 btn.addEventListener('click', function(e) {
                     e.preventDefault();
-                    btn.parentElement.parentElement.parentElement.querySelector('.overlay').classList.add(
+                    console.log(btn.parentElement.parentElement.parentElement.parentElement)
+                    btn.parentElement.parentElement.parentElement.classList.add(
                         'overlay-block');
                     btn.parentElement.parentElement.parentElement.querySelector('.overlay-layer').classList
                         .remove('d-none');
                     isAnnual = (document.querySelector('.active[data-kt-plan]').getAttribute('data-kt-plan') ===
                         'annual' ? true : false)
                     planId = btn.getAttribute('data-plan-id');
-                    quantity = document.querySelector('#amount-' + planId).value
+                    quantity = document.querySelector('#amount').value
 
                     axios.post("{{ route('payment.makeCheckoutLink', $currentAccount->id) }}", {
                         'account': {{ $currentAccount->id }},
@@ -36,9 +37,7 @@
                             'quantity': quantity
                         }
                     }).then(function(res) {
-                        Paddle.Checkout.open({
-                            override: res.data.link
-                        });
+                        location.href = res.data.link.url;
                     }).catch(function(err) {
                         toastr.options = {
                             "closeButton": false,
@@ -62,14 +61,58 @@
                         //TODO: Show message that there is an error with the request
                         console.log(err);
 
-                    }).finally(function(){
-                        btn.parentElement.parentElement.parentElement.querySelector('.overlay')
-                            .classList.remove('overlay-block');
+                    }).finally(function() {
+                        btn.parentElement.parentElement.parentElement.classList.remove('overlay-block');
                         btn.parentElement.parentElement.parentElement.querySelector('.overlay-layer')
                             .classList.add('d-none');
                     });
                 })
             })
+
+            var plans = {!! json_encode($plans) !!};
+
+            function createSliderValues(values) {
+                const steps = 100 / values.length;
+                let sliderValues = {};
+                values.forEach(function(value, index) {
+                    if (index === 0) {
+                        sliderValues['min'] = parseInt(value);
+                    } else if (index === values.length - 1) {
+                        sliderValues['max'] = parseInt(value);
+                    } else {
+                        sliderValues[(index * steps) + '%'] = parseInt(value);
+                    }
+                })
+                return sliderValues;
+            }
+
+            var slider = document.querySelector("#kt_slider_basic");
+
+            noUiSlider.create(slider, {
+                start: 100 / plans.length,
+                snap: true,
+                tooltips: [wNumb({
+                    decimals: 0,
+                    thousand: ',',
+
+                })],
+                connect: true,
+                range: createSliderValues(plans.map(function(plan) {
+                    return plan.contacts
+                }))
+            });
+
+            slider.noUiSlider.on("update", function(values, handle) {
+                var plan = plans.find(function(plan) {
+                    return plan.contacts == values[handle];
+                });
+                document.querySelector('#plan-name').textContent = plan.name;
+                var price = document.querySelector('[data-kt-element="price"]');
+                price.setAttribute('data-kt-plan-price-month', plan.monthly_price);
+                price.setAttribute('data-kt-plan-price-annual', plan.yearly_price);
+                price.textContent = document.querySelector('[data-kt-buttons="true"] .active[data-kt-plan]').getAttribute('data-kt-plan') == 'month' ? price.innerHTML = plan.monthly_price : price.innerHTML = plan.yearly_price;
+                document.querySelector('.purchase-button').setAttribute('data-plan-id', plan.id);
+            });
         </script>
     @endpush
     <div class="post d-flex flex-column-fluid" id="kt_post">
@@ -79,51 +122,24 @@
             <div class="card" id="kt_pricing">
                 <!--begin::Card body-->
                 <div class="card-body p-lg-17">
-                    <!--begin::Plans-->
-                    <div class="d-flex flex-column">
-                        <!--begin::Heading-->
-                        <div class="mb-13 text-center">
-                            <h1 class="fs-2hx fw-bolder mb-5">Choose Your Plan</h1>
-                            <div class="text-gray-400 fw-bold fs-5">If you need more info about our pricing, please check
-                                <a href="#" class="link-primary fw-bolder">Pricing Guidelines</a>.
+
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="nav-group nav-group-outline mx-auto mb-15 d-flex" data-kt-buttons="true">
+                                <a href="#"
+                                    class="flex-fill btn btn-color-gray-400 btn-active btn-active-secondary px-6 py-3 me-2 active"
+                                    data-kt-plan="month">Monthly</a>
+                                <a href="#"
+                                    class="flex-fill btn btn-color-gray-400 btn-active btn-active-secondary px-6 py-3"
+                                    data-kt-plan="annual">Annual</a>
+                            </div>
+                            <div class="mb-0">
+                                <label class="form-label">Contacts</label>
+                                <div id="kt_slider_basic"></div>
                             </div>
                         </div>
-                        <!--end::Heading-->
-                        <!--begin::Nav group-->
-                        <div class="nav-group nav-group-outline mx-auto mb-15" data-kt-buttons="true">
-                            <a href="#"
-                                class="btn btn-color-gray-400 btn-active btn-active-secondary px-6 py-3 me-2 active"
-                                data-kt-plan="month">Monthly</a>
-                            <a href="#" class="btn btn-color-gray-400 btn-active btn-active-secondary px-6 py-3"
-                                data-kt-plan="annual">Annual</a>
-                        </div>
-                        <!--end::Nav group-->
-                        <!--begin::Row-->
-                        <div class="row g-10">
-                            <ul class="nav nav-pills justify-content-center">
-                                <li class="nav-item flex-fill text-center">
-                                    <a class="nav-link active" data-bs-toggle="tab" href="#tab-0">SME Plans</a>
-                                </li>
-                                <li class="nav-item flex-fill text-center">
-                                    <a class="nav-link" data-bs-toggle="tab" href="#tab-1">Enterprise Plans</a>
-                                </li>
-                            </ul>
-                            <div class="tab-content" id="pricing-plans">
-                                @foreach ($plans->chunk(3) as $index => $plansGroup)
-                                    <div class="tab-pane fade show <?php echo $index == 0 ? 'active' : ''; ?>" id="tab-{{ $index }}"
-                                        role="tabpanel">
-                                        <div class="row g-10">
-                                            @foreach ($plansGroup as $plan)
-                                                @include('payment.partials.plan', ['plan' => $plan])
-                                            @endforeach
-                                        </div>
-                                    </div>
-                                @endforeach
-                            </div>
-                        </div>
-                        <!--end::Row-->
+                        @include('payment.partials.plan', ['plan' => $plans->first()])
                     </div>
-                    <!--end::Plans-->
                 </div>
                 <!--end::Card body-->
             </div>
