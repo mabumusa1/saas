095370c2f1dcf281d91156d66688bc85a18f9d4a
Salem Tarek <salemcode8@gmail.com>
2022-03-18 16:46:33 +0000

    BillingControllerTest

---
+++ database/factories/PlanFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Plan>
+ */
+class PlanFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'display_name' => $this->faker->name,
+            'short_description' => $this->faker->sentence,
+            'stripe_product_id' => $this->faker->uuid,
+        ];
+    }
+}
