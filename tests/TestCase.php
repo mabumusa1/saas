837f2c933e68353fa7f9d4a59d6f6ff88f1a1de4
Salem Tarek <salemcode8@gmail.com>
2022-03-21 06:04:52 +0000

    Remove Unused code

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,17 +7,4 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-
-    public function mockStripe()
-    {
-        $this->mock(Stripe::class, function ($mock) {
-            $mock->shouldReceive('setApiKey');
-        });
-
-        $this->mock(Customer::class, function ($mock) {
-            $mock->shouldReceive('create')->andReturn([
-                'id' => 'test_stripe_customer_id',
-            ]);
-        });
-    }
 }
