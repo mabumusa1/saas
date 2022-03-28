9a3a638b18b32d79ea428d5781a93e23a63f5cb4
Salem Tarek <salemcode8@gmail.com>
2022-03-28 14:38:57 +0000

    Invoice Template

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -9,6 +9,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
+use Laravel\Cashier\Invoice;
+use stdClass;
+use Symfony\Component\HttpFoundation\Response;
 
 class BillingController extends Controller
 {
@@ -74,9 +77,20 @@ class BillingController extends Controller
 
     public function invoice(Account $account, $invoiceId)
     {
-        return $account->downloadInvoice($invoiceId, [
+        // return $account->subscriptions->first();
+        // return $account->subscriptions->first()->previewInvoice('price_1KYcdZJJANQIX4AvM2ySzZzb');
+        // return view('cashier::receipt', ['invoice' => $account->findInvoice($invoiceId, [
+        //     'vendor' => 'Your Company',
+        //     'product' => 'Your Product',
+        // ])]);
+        return new Response($account->findInvoice($invoiceId, [
             'vendor' => 'Your Company',
             'product' => 'Your Product',
+        ])->pdf([
+            'vendor' => 'Your Company',
+            'product' => 'Your Product',
+        ]), 200, [
+            'Content-Type' => 'application/pdf',
         ]);
     }
 }
