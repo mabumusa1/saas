e0633b0bb38c003e7624a57e2ab34e6adc002d18
Mohammad Abu Musa <m.abumusa@gmail.com>
2022-03-29 23:11:28 +0000

    fixing event files

--- app/Events/AccountUpdatedEvent.php
+++ app/Events/AccountUpdatedEvent.php
@@ -4,7 +4,6 @@ namespace App\Events;
 
 use App\Models\Account;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
