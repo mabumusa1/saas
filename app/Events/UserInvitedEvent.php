0f978ddb2ecffa667f38915ea98b86f1d1132195
Mohammad Abu Musa <m.abumusa@gmail.com>
2022-03-29 23:11:28 +0000

    fixing event files

--- app/Events/UserInvitedEvent.php
+++ app/Events/UserInvitedEvent.php
@@ -3,7 +3,6 @@
 namespace App\Events;
 
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
