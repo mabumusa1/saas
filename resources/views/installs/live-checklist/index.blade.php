40b9b473a82ae2dcc30edab49f3cb326a4713829
Salem Tarek <salemcode8@gmail.com>
2022-04-09 20:25:36 +0000

    Fix #182

---
+++ resources/views/installs/live-checklist/index.blade.php
@@ -0,0 +1,10 @@
+@extends('installs.layout.default')
+@section('install_module')
+    <div class="card">
+        <div class="card-header">
+            <h2 class="mb-0 card-title">{{ __('Go Live checklist') }}</h2>
+        </div>
+        <div class="card-body">
+        </div>
+    </div>
+@endsection
---
+++ resources/views/partials/general/_coming-soon.blade.php
@@ -0,0 +1,4 @@
+<div class="alert alert-success text-center p-10">
+    <h1>Coming Soon...</h1>
+</div>
+
