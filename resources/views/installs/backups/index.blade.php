5e4cde506902f38947fb2cef2ad16d2a5fdc385f
Salem Tarek <salemcode8@gmail.com>
2022-04-10 16:44:45 +0000

    Fix #183

--- resources/views/installs/backups/index.blade.php
+++ resources/views/installs/backups/index.blade.php
@@ -4,7 +4,7 @@
         <div class="card-header">
             <h2 class="mb-0 card-title">{{ __('Backup Points') }}</h2>
             <div class="card-toolbar">
-                <button class="btn btn-sm border-square btn-dark me-1">Restore</button>
+                <button id="btn-restore" class="btn btn-sm border-square btn-dark me-1">Restore</button>
                 <button class="btn btn-sm border-square btn-dark me-1">Download ZIP</button>
                 <button class="btn btn-sm border-square btn-primary"
                 data-bs-toggle="modal" data-bs-target="#backup_install_modal"
@@ -15,22 +15,22 @@
             <table class="table">
                 <thead>
                     <tr class="fw-bold fs-4 bg-gray-400">
-                        <th></th>
+                        <th>&nbsp;&nbsp;&nbsp;</th></th>
                         <th>Date and time (UTC)</th>
                         <th>Description</th>
-                        <th>ID</th>
+                        <th class="text-start ps-0 pe-3">ID</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach($backups as $backup)
                     <tr>
-                        <td></td>
+                        <td><input type="radio" name="backup_id" @if($loop->first) checked @endif value="{{ $backup->id }}" data-href="{{ route('backups.restore', ['account' => $account, 'install' => $install, 'site' => $site, 'backup' => $backup]) }}"/></td>
                         <td>
                             {{ $backup->created_at->format('m/d/Y h:i:s A') }}
                             <span class="text-muted">{{ $backup->created_at->diffForHumans() }}</span>
                         </td>
                         <td>{{ $backup->description }}</td>
-                        <td>{{ $backup->id }}</td>
+                        <td class="text-start">{{ $backup->id }}</td>
                     </tr>
                     @endforeach
                 </tbody>
@@ -68,5 +68,18 @@
             </div>
         </div>
     </div>
-
+    <form id="restore_form" method="POST">
+        @csrf
+    </form>
 @endsection
+@push('scripts')
+<script>
+    document.querySelector('#btn-restore')
+    .addEventListener('click', function(){
+        var backup_id = document.querySelector('input[name="backup_id"]:checked').getAttribute('data-href');
+        var restoreForm = document.querySelector('#restore_form')
+        restoreForm.setAttribute('action', backup_id);
+        restoreForm.submit();
+    })
+</script>
+@endpush
