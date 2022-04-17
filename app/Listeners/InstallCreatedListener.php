d23fb496a96c34a1f60b74d307599d72cb61d2ea
Mohammad Abu Musa <m.abumusa@gmail.com>
2022-04-17 12:48:42 +0000

    make api call

--- app/Listeners/InstallCreatedListener.php
+++ app/Listeners/InstallCreatedListener.php
@@ -2,23 +2,59 @@
 
 namespace App\Listeners;
 
+use App\Events\InstallCreated;
 use App\Models\Contact;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Http;
 
-class InstallCreatedListener
+class InstallCreatedListener implements ShouldQueue
 {
+    use InteractsWithQueue;
+
+    /**
+     * The number of times the queued listener may be attempted.
+     *
+     * @var int
+     */
+    public $tries = 5;
+
     /**
      * Handle the event.
      *
      * @param  object  $event
      * @return void
      */
-    public function handle($event)
+    public function handle(InstallCreated $event)
     {
-        Contact::create([
-            'install_id' => $event->install->id,
-            'first_name' => $event->user->first_name,
-            'last_name' => $event->user->last_name,
-            'email' => $event->user->email,
+        $response = Http::withHeaders([
+            'X-API-Key' => env('KUB8_API_KEY'),
+        ])
+        ->post(env('KUB8_API')."install/{$event->install->name}", [
+            'env_type' => $event->install->type,
+            'size' => $event->install->size,
+            'domain' => $event->install->domain,
+            'region' => $event->install->region,
+          /*
+           * This will be used i
+           * the future, keep it
+           */
+            /*'custom' => [
+                'cpu' => '1',
+                'memory' => '4',
+            ],*/
         ]);
+    }
+
+    /**
+     * Handle a job failure.
+     *
+     * @param  \App\Events\InstallCreated  $event
+     * @param  \Throwable  $exception
+     * @return void
+     */
+    public function failed(InstallCreated $event, $exception)
+    {
+        //
     }
 }
