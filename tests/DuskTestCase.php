f2b6927d9aad421cf0d706443b9c42c5b4af1555
Mohammad Abu Musa <m.abumusa@gmail.com>
2022-04-30 15:27:29 +0000

    rebase to dev

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -2,6 +2,15 @@
 
 namespace Tests;
 
+use App\Models\Account;
+use App\Models\Backup;
+use App\Models\Cashier\Subscription;
+use App\Models\Contact;
+use App\Models\Domain;
+use App\Models\Install;
+use App\Models\Plan;
+use App\Models\Site;
+use App\Models\User;
 use Facebook\WebDriver\Chrome\ChromeOptions;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
@@ -10,6 +19,60 @@ use Laravel\Dusk\TestCase as BaseTestCase;
 abstract class DuskTestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected User $user;
+
+    protected Account $account;
+
+    protected Site $site;
+
+    protected Install $install;
+
+    protected Contact $contact;
+
+    protected Domain $domain;
+
+    protected Backup $backup;
+
+    public function setUp():void
+    {
+        parent::setUp();
+    }
+
+    public function addAccount()
+    {
+        $this->user = User::factory()->create();
+        $this->account = Account::factory()->create();
+        $this->account->users()->sync([$this->user->id => ['role' => 'owner']]);
+        $this->actingAs($this->user);
+    }
+
+    public function addSite($addDomain = false)
+    {
+        if (empty($this->account)) {
+            $this->addAccount();
+        }
+        $this->site = Site::factory()->for($this->account)->create();
+        $this->install = Install::factory()->for($this->site)->create(['name' => 'domain']);
+        $this->contact = Contact::factory()->for($this->install)->create();
+
+        if ($addDomain) {
+            $this->domain = Domain::factory()
+            ->for($this->install)
+            ->create(['name' => 'domain.steercampaign.com', 'primary' => true, 'verified_at' => null]);
+        }
+    }
+
+    public function addBackup()
+    {
+        if (empty($this->install)) {
+            $this->addSite();
+        }
+
+        $this->backup = Backup::factory()
+        ->for($this->install)
+        ->create();
+    }
 
     /**
      * Prepare for Dusk test execution.
