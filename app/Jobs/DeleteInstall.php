a6ddac0105274e7cccb788887aef114146227bf3
Mohammad Abu Musa <m.abumusa@gmail.com>
2022-06-15 14:27:00 +0000

    added logging to kub8 calls

--- app/Jobs/DeleteInstall.php
+++ app/Jobs/DeleteInstall.php
@@ -10,6 +10,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Log;
 
 class DeleteInstall implements ShouldQueue
 {
@@ -35,5 +36,12 @@ class DeleteInstall implements ShouldQueue
     public function handle()
     {
         $response = Http::kub8()->delete("install/{$this->install->name}");
+        $response->onError(function ($response) {
+            /* @var Response $response */
+            Log::emergency(
+                'Kub8 Request Failed '.$response->getStatusCode().' : '.
+             \json_encode($response->body().' Orgianl Request: '.$this->install->name)
+            );
+        });
     }
 }
--- tests/Feature/Jobs/Kub8JobTest.php
+++ tests/Feature/Jobs/Kub8JobTest.php
@@ -10,6 +10,7 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\Client\Request;
 use Illuminate\Support\Facades\Bus;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Queue;
 use Tests\TestCase;
 
@@ -21,15 +22,40 @@ class Kub8JobTest extends TestCase
     {
         parent::setUp();
         parent::addSite(true);
-        Http::fake([
-            env('KUB8_API').'install/create' => Http::response([], 200),
-            env('KUB8_API').'install/copy' => Http::response([], 200),
-            env('KUB8_API')."install/{$this->install->name}" => Http::response([], 200),
-        ]);
     }
 
     public function test_create_install_job():void
     {
+        Http::fake([
+            env('KUB8_API').'install/create' => Http::response([], 200),
+        ]);
+
+        $job = new CreateInstall($this->install);
+        $job->handle();
+        Http::assertSent(function (Request $request) {
+            return $request->hasHeader('Authorization', 'Basic '.\base64_encode(env('KUB8_USERNAME').':'.env('KUB8_PASSWORD'))) &&
+                   $request->url() == env('KUB8_API').'install/create' &&
+                   $request->method() == 'POST' &&
+                   $request['id'] == $this->install->name &&
+                   $request['env_type'] == $this->install->type &&
+                   $request['size'] == $this->install->size &&
+                   $request['domain'] == $this->install->domain &&
+                   $request['region'] == $this->install->region;
+        });
+    }
+
+    public function test_create_install_job_fail():void
+    {
+        Http::fake([
+            env('KUB8_API').'install/create' => Http::response([
+                'status' => 'error',
+                'message' =>'statefulsets.apps "recorder3" not found',
+            ], 413),
+        ]);
+
+        Log::shouldReceive('emergency')
+        ->once();
+
         $job = new CreateInstall($this->install);
         $job->handle();
         Http::assertSent(function (Request $request) {
@@ -46,6 +72,33 @@ class Kub8JobTest extends TestCase
 
     public function test_copy_install_job():void
     {
+        Http::fake([
+            env('KUB8_API').'install/copy' => Http::response([], 200),
+        ]);
+
+        $job = new CopyInstall($this->install);
+        $job->handle();
+        Http::assertSent(function (Request $request) {
+            return $request->hasHeader('Authorization', 'Basic '.\base64_encode(env('KUB8_USERNAME').':'.env('KUB8_PASSWORD'))) &&
+                   $request->url() == env('KUB8_API').'install/copy' &&
+                   $request->method() == 'POST' &&
+                   $request['id'] == $this->install->name &&
+                   $request['env_type'] == $this->install->type &&
+                   $request['size'] == $this->install->size &&
+                   $request['domain'] == $this->install->domain &&
+                   $request['region'] == $this->install->region;
+        });
+    }
+
+    public function test_copy_install_job_fail():void
+    {
+        Http::fake([
+            env('KUB8_API').'install/copy' => Http::response([], 500),
+        ]);
+        Log::shouldReceive('emergency')
+        ->once()
+        ->with('Kub8 Request Failed 500 : "[] Orgianl Request: {\"id\":\"domain\",\"env_type\":\"dev\",\"size\":\"s0\",\"domain\":null,\"region\":\"us-east-1\"}"');
+
         $job = new CopyInstall($this->install);
         $job->handle();
         Http::assertSent(function (Request $request) {
@@ -62,6 +115,28 @@ class Kub8JobTest extends TestCase
 
     public function test_delete_install_job():void
     {
+        Http::fake([
+            env('KUB8_API')."install/{$this->install->name}" => Http::response([], 200),
+        ]);
+
+        $job = new DeleteInstall($this->install);
+        $job->handle();
+        Http::assertSent(function (Request $request) {
+            return $request->hasHeader('Authorization', 'Basic '.\base64_encode(env('KUB8_USERNAME').':'.env('KUB8_PASSWORD'))) &&
+                   $request->url() == env('KUB8_API')."install/{$this->install->name}" &&
+                   $request->method() == 'DELETE';
+        });
+    }
+
+    public function test_delete_install_job_fail():void
+    {
+        Http::fake([
+            env('KUB8_API')."install/{$this->install->name}" => Http::response([], 413),
+        ]);
+        Log::shouldReceive('emergency')
+        ->once()
+        ->with('Kub8 Request Failed 413 : "[] Orgianl Request: domain"');
+
         $job = new DeleteInstall($this->install);
         $job->handle();
         Http::assertSent(function (Request $request) {
